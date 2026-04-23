8ba6b34fa3503e9b302ec0388da556e8815745ab
Astro-lad <ibrohim080616@gmail.com>
2026-04-24 03:26:33 +0500

    test just

--- antennacalc/settings.py
+++ antennacalc/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-a$-f6)=$(5-#h%9chhldtc$q_3-(j5cd1w6h(sfmf5(4c1fi)%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['antennacalc.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
